fac0b76636b9edbf31c76be936ac5914c9d78832
CanoCOL <canoloramiguel103@gmail.com>
2022-08-25 14:03:55 -0500

    hola2

--- src/controllers/controladorfactura.js
+++ src/controllers/controladorfactura.js
@@ -5,8 +5,6 @@ let bill = document.getElementById("bill");
 
 //comprobando el estado del carrito de compras
 if (car == null) {
-  console.log("oeee");
-
   let row = document.createElement("div");
   row.classList.add("row", "my-5", "justify-content-center");
 
@@ -20,23 +18,45 @@ if (car == null) {
 
   let message = document.createElement("h3");
   message.classList.add("text-center");
-  message.textContent = "BUY";
+  message.textContent = "Make a Purchase!";
+
+  let a = document.createElement("a");
+  a.setAttribute("href", "../views/coleccion.html");
+  a.classList.add(
+    "btn",
+    "bg-dark",
+    "text-white",
+    "d-flex",
+    "justify-content-center"
+  );
+  let aTexto = document.createTextNode("Back to back");
+  a.appendChild(aTexto);
+
+  // document.body.appendChild(a);
 
   column.appendChild(picture);
   column.appendChild(message);
+  column.appendChild(a);
   row.appendChild(column);
   bill.appendChild(row);
 } else {
   //RECORRO EL CARRITO
   car.forEach(function (product) {
     let row = document.createElement("div");
-    row.classList.add("row", "my-5", "justify-content-center");
+    row.classList.add(
+      "row",
+      "my-4",
+      "justify-content-center",
+      "border",
+      "border-dark",
+      "rounded"
+    );
 
     let column1 = document.createElement("div");
     column1.classList.add("col-12", "col-md-5");
 
     let column2 = document.createElement("div");
-    column2.classList.add("col-12", "col-md-3");
+    column2.classList.add("col-12", "col-md-3", "text-center", "my-auto");
 
     let column3 = document.createElement("div");
     column3.classList.add("col-12", "col-md-3");
@@ -46,16 +66,16 @@ if (car == null) {
     picture.src = product.picture;
 
     let name = document.createElement("h3");
-    name.classList.add("text-conent", "text-muted");
+    name.classList.add("text-content");
     name.textContent = product.name;
 
     let quantity = document.createElement("h3");
     name.classList.add("text-center", "mt-5");
-    quantity.textContent = "unit: " + product.quantity;
+    quantity.textContent = "Unity: " + product.quantity;
 
     let price = document.createElement("h4");
     name.classList.add("text-center", "mt-5");
-    price.textContent = "unit cost " + product.price;
+    price.textContent = "Unity Cost: " + product.price;
 
     let subtotal = document.createElement("h2");
     subtotal.classList.add("fw-bold");
